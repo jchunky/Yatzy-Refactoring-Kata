70e7e893667fdae53c61c5514332c35f238bca69
Chunky <jasonc@nulogy.com>
2020-04-27 01:55:51 -0400

    refactor

--- javascript/yatzy.js
+++ javascript/yatzy.js
@@ -6,31 +6,27 @@ export default class Yatzy {
   }
 
   fours() {
-    return Yatzy.scoreSingles(this.dice, 4);
+    return Yatzy.count(this.dice, 4) * 4;
   }
 
   fives() {
-    return Yatzy.scoreSingles(this.dice, 5);
+    return Yatzy.count(this.dice, 5) * 5;
   }
 
   sixes() {
-    return Yatzy.scoreSingles(this.dice, 6);
+    return Yatzy.count(this.dice, 6) * 6;
   }
 
   static ones(...dice) {
-    return this.scoreSingles(dice, 1);
+    return this.count(dice, 1) * 1;
   }
 
   static twos(...dice) {
-    return this.scoreSingles(dice, 2);
+    return this.count(dice, 2) * 2;
   }
 
   static threes(...dice) {
-    return this.scoreSingles(dice, 3);
-  }
-
-  static scoreSingles(dice, n) {
-    return (_.countBy(dice)[n] || 0) * n;
+    return this.count(dice, 3) * 3;
   }
 
   static chance(...dice) {
@@ -39,6 +35,14 @@ export default class Yatzy {
 
   static yatzy(...dice) {
     return _.uniq(dice).length === 1 ? 50 : 0;
+  }
+
+  static smallStraight(...dice) {
+    return _.isEqual(dice.sort(), [1, 2, 3, 4, 5]) ? _.sum(dice) : 0;
+  }
+
+  static largeStraight(...dice) {
+    return _.isEqual(dice.sort(), [2, 3, 4, 5, 6]) ? _.sum(dice) : 0;
   }
 
   static score_pair(...dice) {
@@ -56,30 +60,15 @@ export default class Yatzy {
   static scoreMultiple(dice, n) {
     const match = _(dice)
       .uniq()
-      .filter(d => _.countBy(dice)[d] >= n)
+      .filter(d => this.count(dice, d) >= n)
       .max();
     return (match || 0) * n;
-  }
-
-  static smallStraight(...dice) {
-    return this.scoreStraight(dice, [1, 2, 3, 4, 5]);
-  }
-
-  static largeStraight(...dice) {
-    return this.scoreStraight(dice, [2, 3, 4, 5, 6]);
-  }
-
-  static scoreStraight(dice, straight) {
-    const match = _(dice)
-      .orderBy()
-      .isEqual(straight);
-    return match ? _.sum(dice) : 0;
   }
 
   static two_pair(...dice) {
     const pairs = _(dice)
       .uniq()
-      .filter(d => _.countBy(dice)[d] >= 2)
+      .filter(d => this.count(dice, d) >= 2)
       .value();
     return pairs.length === 2 ? _.sum(pairs) * 2 : 0;
   }
@@ -87,9 +76,13 @@ export default class Yatzy {
   static fullHouse(...dice) {
     const match = _(dice)
       .uniq()
-      .map(d => _.countBy(dice)[d])
+      .map(d => this.count(dice, d))
       .orderBy()
       .isEqual([2, 3]);
     return match ? _.sum(dice) : 0;
+  }
+
+  static count(dice, n) {
+    return _.countBy(dice)[n] || 0;
   }
 }

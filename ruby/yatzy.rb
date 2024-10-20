a18eaf5956f3f88f41ce303219e4b2d26f68734c
Jason CheongKeeYou <jasonc@nulogy.com>
2024-10-20 06:41:39 -0400

    refactor

--- ruby/yatzy.rb
+++ ruby/yatzy.rb
@@ -6,18 +6,18 @@ class Yatzy
     pairs.size == 2 ? pairs.sum * 2 : 0
   end
 
-  def self.chance(*dice) = dice.sum
-  def self.yatzy(dice) = dice.uniq.size == 1 ? 50 : 0
-  def self.score_pair(*dice) = find_n_of_a_kind(dice, n: 2) * 2
-  def self.three_of_a_kind(*dice) = find_n_of_a_kind(dice, n: 3) * 3
-  def self.four_of_a_kind(*dice) = find_n_of_a_kind(dice, n: 4) * 4
+  def self.chance(*dice)              = dice.sum
+  def self.yatzy(dice)                = dice.uniq.size == 1 ? 50 : 0
+  def self.score_pair(*dice)          = find_n_of_a_kind(dice, n: 2) * 2
+  def self.three_of_a_kind(*dice)     = find_n_of_a_kind(dice, n: 3) * 3
+  def self.four_of_a_kind(*dice)      = find_n_of_a_kind(dice, n: 4) * 4
   def self.find_n_of_a_kind(dice, n:) = dice.select { dice.count(_1) >= n }.max.to_i
-  def self.smallStraight(*dice) = dice.sort == (1..5).to_a ? dice.sum : 0
-  def self.largeStraight(*dice) = dice.sort == (2..6).to_a ? dice.sum : 0
-  def self.fullHouse(*dice) = dice.tally.values.sort == [2, 3] ? dice.sum : 0
-  def self.ones(*dice) = dice.count(1) * 1
-  def self.twos(*dice) = dice.count(2) * 2
-  def self.threes(*dice) = dice.count(3) * 3
+  def self.smallStraight(*dice)       = dice.sort == (1..5).to_a ? dice.sum : 0
+  def self.largeStraight(*dice)       = dice.sort == (2..6).to_a ? dice.sum : 0
+  def self.fullHouse(*dice)           = dice.tally.values.sort == [2, 3] ? dice.sum : 0
+  def self.ones(*dice)                = dice.count(1) * 1
+  def self.twos(*dice)                = dice.count(2) * 2
+  def self.threes(*dice)              = dice.count(3) * 3
 
   def initialize(*dice)
     @dice = dice
